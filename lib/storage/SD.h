559a52f8f2b22fc4dd22a619ce01c0f4f09e852f
Charles <37224242+Charlito33@users.noreply.github.com>
2024-01-18 22:22:57 +0100

    Added C compatibility with C++ compiler

--- lib/storage/SD.h
+++ lib/storage/SD.h
@@ -1,11 +1,19 @@
 #ifndef SD_HPP
 #define SD_HPP 
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdbool.h>
 
 #define MOUNT_POINT "/sdcard"
 
 bool SD_init(void);
 void SD_disconnect(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* SD_HPP */
