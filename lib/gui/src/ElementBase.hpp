6d30672d2345c624dc5e3b6aeb425aaf6ca8992b
Charles <37224242+Charlito33@users.noreply.github.com>
2024-03-13 21:50:43 +0100

    Refactor getLastTouchPos into absolute and relative methods
    Separated the `getLastTouchPos` method into `getLastTouchPosAbs` for absolute position and `getLastTouchPosRel` for relative position in the GUI library. This update includes appropriate documentation comments for both methods.

--- lib/gui/src/ElementBase.hpp
+++ lib/gui/src/ElementBase.hpp
@@ -121,7 +121,8 @@ namespace gui
         static int16_t touchX, touchY;
 
         int16_t m_lastTouchX, m_lastTouchY;
-        void getLastTouchPos(int16_t* x, int16_t* y) const;
+        void getLastTouchPosAbs(int16_t* x, int16_t* y) const;
+        void getLastTouchPosRel(int16_t* x, int16_t* y) const;
 
         std::shared_ptr<graphics::Surface> m_surface; // Surface to render the widget
 
