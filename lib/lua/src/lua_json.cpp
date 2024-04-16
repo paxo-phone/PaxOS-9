f711bc7b1246cae2737d1158383c1cebe58ea0e8
paxo-rch <paxo.rch@gmail.com>
2024-04-17 01:02:21 +0200

    ajout de json dans lua (a tester)

---
+++ lib/lua/src/lua_json.cpp
@@ -0,0 +1,82 @@
+#include "lua_json.hpp"
+
+LuaJson::LuaJson(std::string data)
+    : json(*(new nlohmann::json)),
+      allocated(true)
+{
+    delete &json;   // si quelqu'un arrive a faire mieux :/
+
+    if (nlohmann::json::accept(data))
+        json = nlohmann::json::parse(data);
+    else
+        json = nlohmann::json::parse("{}");
+}
+
+LuaJson::LuaJson(nlohmann::json& other)
+    : json(other)
+{}
+
+std::string LuaJson::get()
+{
+    return json.dump();
+}
+
+LuaJson LuaJson::op(std::string key)
+{
+    return LuaJson(json[key]);
+}
+
+void LuaJson::set(std::string id, std::string value)
+{
+    this->json[id] = value;
+}
+
+bool LuaJson::is_null()
+{
+    return json.is_null();
+}
+
+size_t LuaJson::size()
+{
+    return json.size();
+}
+
+bool LuaJson::has_key(std::string key)
+{
+    return json.find(key) != json.end();
+}
+
+void LuaJson::remove(std::string key)
+{
+    json.erase(key);
+}
+
+int LuaJson::get_int(std::string key)
+{
+    return json[key].get<int>();
+}
+
+double LuaJson::get_double(std::string key)
+{
+    return json[key].get<double>();
+}
+
+bool LuaJson::get_bool(std::string key)
+{
+    return json[key].get<bool>();
+}
+
+void LuaJson::set_int(std::string key, int value)
+{
+    json[key] = value;
+}
+
+void LuaJson::set_double(std::string key, double value)
+{
+    json[key] = value;
+}
+
+void LuaJson::set_bool(std::string key, bool value)
+{
+    json[key] = value;
+}
--- storage/apps/test/app.lua
+++ storage/apps/test/app.lua
@@ -1,21 +1,20 @@
 function run()
     win = gui:window()
+
+    local json_str = '{"name": {"age": 30, "city": "New York"}}'
+    local json_obj = Json:new(json_str)
+
+    print(json_obj:has_key("name")) -- print true
+    print(json_obj:has_key("names")) -- print false
+
+    print(json_obj["name"]:get_int("age")) -- print 30
+    json_obj["name"]["age"] = json_obj["name"]:get_int("age") + 1 -- use :get(void) to get in string
+    print(json_obj["name"]:get_int("age")) -- print 31
     
-    todo_label = gui:label(win, 35, 35, 85, 41)
-    todo_label:setHorizontalAlignment(LEFT_ALIGNMENT)
-    todo_label:setFontSize(36)
-    todo_label:setText("Todo")
-
-    -- tasks = {
-    --     "Vivamus laoreet.",
-    --     "Suspendisse eu ligula.",
-    --     "Curabitur ligula sapien",
-    --     "Praesent egestas tristique"
-    -- }
-
-    tasks_container = gui:box(win, 35, 100, 229, 260)
-    tasks_container:setMainColor(COLOR_DARK)
-
+    todo_label = gui:label(win, 0, 0, 320, 480)
+    todo_label:setHorizontalAlignment(CENTER_ALIGNMENT)
+    todo_label:setFontSize(15)
+    todo_label:setText(json_obj:get())
 
     gui:setWindow(win)
 end
