2c744bd6e546fc5af4473a13d73e6b62a6c691d8
paxo-rch <paxo.rch@gmail.com>
2024-04-18 10:57:58 +0200

    flip screen

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ void setup()
     graphics::init();
     storage::init();
 
-    graphics::setScreenOrientation(graphics::LANDSCAPE);
+    graphics::setScreenOrientation(graphics::PORTRAIT);
 
     ThreadManager::init();
 
