d16149cf9b1c74d229f77f0abcb92795ab2e591b
Antoine Bollengier <44288655+b5i@users.noreply.github.com>
2024-06-29 15:07:23 +0200

    Fixed compilation for other operating systems.

--- lib/lua/src/SOL2/sol.hpp
+++ lib/lua/src/SOL2/sol.hpp
@@ -19673,7 +19673,11 @@ namespace sol { namespace function_detail {
 		}
 
 		template <bool is_yielding, bool no_trampoline>
+		#if defined(__APPLE__)
 		static int call(lua_State* L) {
+		#else
+		static int call(lua_State* L) noexcept(std::is_nothrow_copy_assignable_v<T>)
+		#endif
 			int nr;
 			if constexpr (no_trampoline) {
 				nr = real_call(L);
@@ -19713,7 +19717,11 @@ namespace sol { namespace function_detail {
 		}
 
 		template <bool is_yielding, bool no_trampoline>
+		#if defined(__APPLE__)
 		static int call(lua_State* L) {
+		#else
+		static int call(lua_State* L) noexcept(std::is_nothrow_copy_assignable_v<T>)
+		#endif
 			int nr;
 			if constexpr (no_trampoline) {
 				nr = real_call(L);
