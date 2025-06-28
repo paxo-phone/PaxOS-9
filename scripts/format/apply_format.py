06ba58ac3cbdef6ba79a72e4832d552c61424950
Charles Mahoudeau <charles@mahoudeau.eu>
2025-06-28 18:09:07 +0200

    chore: Fix apply_format.py to use forward slashes only.

--- scripts/format/apply_format.py
+++ scripts/format/apply_format.py
@@ -3,19 +3,23 @@ import os
 import shutil
 import subprocess
 
-SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-PROJECT_DIR = os.path.dirname(os.path.dirname(SCRIPT_DIR))
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
+PROJECT_DIR = os.path.dirname(os.path.dirname(SCRIPT_DIR)).replace("\\", "/")
+CLANG_FORMAT_FILE = os.path.join(PROJECT_DIR, ".clang-format").replace("\\", "/")
+CLANG_FORMAT_IGNORE_FILE = os.path.join(PROJECT_DIR, ".clang-format-ignore").replace("\\", "/")
 
 
 def get_clang_format_path():
     clang_format = shutil.which("clang-format")
     if not clang_format:
         raise EnvironmentError("clang-format not found in PATH. Please install it or add it to your PATH.")
+    if not os.path.isfile(CLANG_FORMAT_FILE):
+        raise FileNotFoundError(f".clang-format file not found at {CLANG_FORMAT_FILE}. Please create one.")
     return clang_format
 
 
 def get_ignore_list():
-    with open(os.path.join(PROJECT_DIR, ".clang-format-ignore"), "r") as f:
+    with open(CLANG_FORMAT_IGNORE_FILE, "r") as f:
         ignore_list = [line.strip() for line in f if line.strip() and not line.startswith("#")]
     return ignore_list
 
@@ -37,18 +41,18 @@ def get_files():
         for file in files:
             if not is_source_file(file):
                 continue
-            source_path = os.path.relpath(str(os.path.join(path, file)), PROJECT_DIR)
+            source_path = os.path.relpath(str(os.path.join(path, file)), PROJECT_DIR).replace("\\", "/")
             output.append(source_path)
     return output
 
 
 def format_files(formatter_path, paths):
     for file in paths:
-        full_path = os.path.join(PROJECT_DIR, file)
+        full_path = os.path.join(PROJECT_DIR, file).replace("\\", "/")
         subprocess.run(
             [
                 formatter_path,
-                f"--style=file:{os.path.join(PROJECT_DIR, ".clang-format")}",
+                f"--style=file:{CLANG_FORMAT_FILE}",
                 "-i",
                 full_path
             ],
@@ -61,8 +65,8 @@ if __name__ == "__main__":
     os.chdir(PROJECT_DIR)
     clang_format_path = get_clang_format_path()
     print("Using clang-format at:", clang_format_path)
-    files = get_files()
+    target_files = get_files()
     ignore_files = get_ignore_list()
-    files = [file for file in files if not is_file_ignored(ignore_files, file)]
-    format_files(clang_format_path, files)
-    print(f"Done, formatted {len(files)} files.")
+    target_files = [file for file in target_files if not is_file_ignored(ignore_files, file)]
+    format_files(clang_format_path, target_files)
+    print(f"Done, formatted {len(target_files)} files.")
